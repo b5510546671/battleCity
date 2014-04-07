b539ca592aed6235bfb6727ce259a83d6e3cb111
Supavit Kongwudhikunakorn <supavit_knot@yahoo.co.th>
2014-04-07 23:38:28 +0700

    able to show 'game over' when game ends

--- src/GameOverLayer.js
+++ src/GameOverLayer.js
@@ -9,6 +9,11 @@ var GameOverLayer = cc.LayerColor.extend({
         this._super( new cc.Color4B( 0, 0, 0, 0 ) );
         this.setOpacity( 100 );
         this.setPosition( new cc.Point( 0, 0 ) );
+
+        this.gameOverLabel = cc.LabelTTF.create( 'GAME OVER!', 'Arial', 60 );
+        this.gameOverLabel.setPosition( new cc.Point( 10*40 + 20, 6*40 + 20 ) );
+        this.addChild( this.gameOverLabel );
+
     },
     
 });
