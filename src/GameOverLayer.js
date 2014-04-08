3959b216ddc3549aac713643c1cde429e13991fe
Supavit Kongwudhikunakorn <supavit_knot@yahoo.co.th>
2014-04-08 19:38:29 +0700

    add scores to game over screen

--- src/GameOverLayer.js
+++ src/GameOverLayer.js
@@ -11,8 +11,14 @@ var GameOverLayer = cc.LayerColor.extend({
         this.setPosition( new cc.Point( 0, 0 ) );
 
         this.gameOverLabel = cc.LabelTTF.create( 'GAME OVER!', 'Arial', 60 );
-        this.gameOverLabel.setPosition( new cc.Point( 10*40 + 20, 6*40 + 20 ) );
+        this.gameOverLabel.setPosition( new cc.Point( 10*40 + 20, 6*40 + 120 ) );
+        
+        this.scoreLabel = cc.LabelTTF.create( 'SCORES : ', 'Arial', 60 );
+        this.scoreLabel.setPosition( new cc.Point( 10*40 , 6*40 + 20 ) );
+        
+        
         this.addChild( this.gameOverLabel );
+        this.addChild( this.scoreLabel );
 
     },
     
